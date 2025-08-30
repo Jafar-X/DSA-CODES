e602cf27e696ac59573280d62eef5050926d08b0
jafar-x <jafarsadikdev@gmail.com>
2025-08-30 22:12:36 +0600

    add binary search tree and preOrder, inOrder, and postOrder traversals

--- Tree/src/BinaryTree.java
+++ Tree/src/BinaryTree.java
@@ -56,10 +56,55 @@ class BinaryTree {
         display(node.right, indent + "\t");
     }
 
+    //traversal
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+        if(node == null) {
+            return;
+        }
+
+        System.out.println(node.value + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    private void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.println(node.value + " ");
+        inOrder(node.right);
+
+    }
+
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    private void postOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.value + " ");
+
+    }
+
     public static void main(String[] args) {
         BinaryTree binaryTree = new BinaryTree();
         Scanner scanner = new Scanner(System.in);
         binaryTree.populate(scanner);
+        binaryTree.preOrder();
         binaryTree.display();
     }
 }
